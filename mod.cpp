52899a65f17a0ab8631569801f45c0392979da3c
Tom Campbell <tyen901@gmail.com>
2024-12-16 13:04:41 +1300

    Initial commit

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Tyen Customs - Tactical Breach Wizards";
+picture = "logo.paa";
+logo = "logo.paa";
+logoOver = "logo.paa";
+logoSmall = "logo_small.paa";
+overview = "";
+hideName = 0;
+hidePicture = 0;
